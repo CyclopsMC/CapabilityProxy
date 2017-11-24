2f6c6aab57d3e3266906d7261c760592bb49e61c
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-11-23 23:20:27 -0500

    Proxy right-clicks

--- src/main/java/org/cyclops/capabilityproxy/block/BlockEntityCapabilityProxy.java
+++ src/main/java/org/cyclops/capabilityproxy/block/BlockEntityCapabilityProxy.java
@@ -1,15 +1,24 @@
 package org.cyclops.capabilityproxy.block;
 
 import com.google.common.collect.Lists;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+
+import java.util.List;
+
 import org.cyclops.capabilityproxy.tileentity.TileEntityCapabilityProxy;
 import org.cyclops.cyclopscore.block.property.BlockProperty;
 import org.cyclops.cyclopscore.config.configurable.ConfigurableBlockContainer;
@@ -51,5 +60,23 @@ public class BlockEntityCapabilityProxy extends ConfigurableBlockContainer {
                                             int meta, EntityLivingBase placer, EnumHand hand) {
         return this.getDefaultState()
                 .withProperty(FACING, facing.getOpposite());
+    }
+
+    @Override
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn,
+                                    EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+        EnumFacing facing = state.getValue(FACING);
+        if(side == facing) return false; // In the future, this will be how you open the filter GUI
+        BlockPos targetPos = pos.offset(facing);
+        List<Entity> entities = worldIn.getEntitiesWithinAABB(Entity.class, new AxisAlignedBB(targetPos));
+        for(Entity entity : entities) {
+            EnumActionResult result = entity.applyPlayerInteraction(playerIn, new Vec3d(targetPos.getX() + 0.5 - entity.posX, targetPos.getY() + 0.5 - entity.posY, targetPos.getZ() + 0.5 - entity.posZ), hand);
+            if(result != EnumActionResult.PASS)
+                return true;
+            result = playerIn.interactOn(entity, hand);
+            if(result != EnumActionResult.PASS)
+                return true;
+        }
+        return false;
     }
 }
