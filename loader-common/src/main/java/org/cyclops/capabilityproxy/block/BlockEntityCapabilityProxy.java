029a3ccb0a95b83d9d573e1a831e3a442cfc8efc
Ruben Taelman <rubensworks@users.noreply.github.com>
2026-09-20 13:30:59 +0200

    Update to Minecraft 26.3 (#55)

--- loader-common/src/main/java/org/cyclops/capabilityproxy/block/BlockEntityCapabilityProxy.java
+++ loader-common/src/main/java/org/cyclops/capabilityproxy/block/BlockEntityCapabilityProxy.java
@@ -1,6 +1,5 @@
 package org.cyclops.capabilityproxy.block;
 
-import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionResult;
@@ -8,9 +7,7 @@ import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -31,19 +28,13 @@ import java.util.function.BiFunction;
  */
 public class BlockEntityCapabilityProxy extends BlockWithEntity {
 
-    public final MapCodec<BlockEntityCapabilityProxy> codec;
 
     public static final EnumProperty<Direction> FACING = BlockStateProperties.FACING;
 
     public BlockEntityCapabilityProxy(Block.Properties properties, BiFunction<BlockPos, BlockState, ? extends CyclopsBlockEntity> blockEntitySupplier) {
         super(properties, blockEntitySupplier);
-        this.codec = BlockBehaviour.simpleCodec(p -> new BlockEntityCapabilityProxy(p, blockEntitySupplier));
     }
 
-    @Override
-    protected MapCodec<? extends BaseEntityBlock> codec() {
-        return codec;
-    }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
--- loader-common/src/main/java/org/cyclops/capabilityproxy/block/BlockItemCapabilityProxy.java
+++ loader-common/src/main/java/org/cyclops/capabilityproxy/block/BlockItemCapabilityProxy.java
@@ -1,12 +1,9 @@
 package org.cyclops.capabilityproxy.block;
 
-import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -25,20 +22,14 @@ import java.util.function.BiFunction;
  */
 public class BlockItemCapabilityProxy extends BlockWithEntityGui {
 
-    public final MapCodec<BlockItemCapabilityProxy> codec;
 
     public static final EnumProperty<Direction> FACING = BlockStateProperties.FACING;
     public static final BooleanProperty INACTIVE = BooleanProperty.create("inactive");
 
     public BlockItemCapabilityProxy(Block.Properties properties, BiFunction<BlockPos, BlockState, ? extends CyclopsBlockEntity> blockEntitySupplier) {
         super(properties, blockEntitySupplier);
-        this.codec = BlockBehaviour.simpleCodec(p -> new BlockItemCapabilityProxy(p, blockEntitySupplier));
     }
 
-    @Override
-    protected MapCodec<? extends BaseEntityBlock> codec() {
-        return codec;
-    }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
